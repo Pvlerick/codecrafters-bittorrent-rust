003a9cca9285fc51162ce06f566210bd77a609c8
Philippe Vlérick <pvlerick@gmail.com>
2024-10-03 13:23:18 +0200

    NewType for formatting

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,30 @@
 use serde_json;
 use std::env;
 
-// Available if you need it!
-// use serde_bencode
+#[derive(Debug)]
+struct Value(serde_json::Value);
+
+impl ToString for Value {
+    fn to_string(&self) -> String {
+        match &self.0 {
+            serde_json::Value::String(_) => self.0.to_string(),
+            serde_json::Value::Number(n) => n.as_i64().expect("cannot unwrap i64").to_string(),
+            _ => todo!(),
+        }
+    }
+}
 
 #[allow(dead_code)]
-fn decode_bencoded_value(encoded_value: &str) -> serde_json::Value {
+fn decode_bencoded_value(encoded_value: &str) -> Value {
     match encoded_value.chars().next() {
-        Some(c) if c.is_digit(10) => serde_json::Value::String(
+        Some(c) if c.is_digit(10) => Value(serde_json::Value::String(
             serde_bencode::from_str(encoded_value).expect("cannot decode string"),
+        )),
+        Some('i') => Value(
+            serde_bencode::from_str::<i64>(encoded_value)
+                .expect("cannot decode number")
+                .into(),
         ),
-        Some('i') => serde_bencode::from_str::<i64>(encoded_value)
-            .expect("cannot decode number")
-            .into(),
         _ => panic!("Unhandled encoded value: {}", encoded_value),
     }
 }
@@ -24,14 +36,7 @@ fn main() {
     if command == "decode" {
         let encoded_value = &args[2];
         let decoded_value = decode_bencoded_value(encoded_value);
-        let decoded_value = match decode_bencoded_value(encoded_value) {
-            serde_json::Value::String(_) => decoded_value.to_string(),
-            serde_json::Value::Number(val) => {
-                val.as_i64().expect("cannot unwrap as i64").to_string()
-            }
-            _ => panic!("unsupported value"),
-        };
-        println!("{}", decoded_value);
+        println!("{}", decoded_value.to_string());
     } else {
         println!("unknown command: {}", args[1])
     }
@@ -44,18 +49,18 @@ mod test {
     #[test]
     fn decode_string() {
         let val = decode_bencoded_value("5:hello");
-        assert_eq!("hello", val);
+        assert_eq!(r#""hello""#, val.to_string());
     }
 
     #[test]
     fn decode_positive_int() {
         let val = decode_bencoded_value("i52e");
-        assert_eq!("52", val.as_i64().unwrap().to_string());
+        assert_eq!("52", val.to_string());
     }
 
     #[test]
     fn decode_negative_int() {
         let val = decode_bencoded_value("i-42e");
-        assert_eq!("-42", val.as_i64().unwrap().to_string());
+        assert_eq!("-42", val.to_string());
     }
 }
