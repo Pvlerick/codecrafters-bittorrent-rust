d7784634faa7e8411b3b6969ff9e5b54dec11bee
Philippe Vlérick <pvlerick@gmail.com>
2024-10-07 16:12:47 +0200

    display for dict

--- src/main.rs
+++ src/main.rs
@@ -140,7 +140,7 @@ fn main() {
     if command == "decode" {
         let encoded_value = &args[2];
         let decoded_value = decode_bencoded_value(encoded_value);
-        println!("{}", decoded_value.to_string());
+        println!("{}", display(&decoded_value));
     } else {
         println!("unknown command: {}", args[1])
     }
