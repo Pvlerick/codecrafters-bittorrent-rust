246fd49f817b45998a06bcece48e61bdb898de3b
Philippe Vlérick <pvlerick@gmail.com>
2024-10-08 10:54:17 +0200

    refactoring in an iterator

--- src/main.rs
+++ src/main.rs
@@ -1,146 +1,165 @@
-use serde_json::{Map, Value};
-use std::env;
+use std::{collections::HashMap, env, fmt::Display};
+
+const NUMBER_HEADER: u8 = b'i';
+const NUMBER_TRAILER: u8 = b'e';
+const LIST_HEADER: u8 = b'l';
+const LIST_TRAILER: u8 = b'e';
+const DICT_HEADER: u8 = b'd';
+const DICT_TRAILER: u8 = b'e';
 
 #[allow(dead_code)]
-fn display(value: &Value) -> String {
-    match value {
-        Value::String(s) => format!("\"{}\"", s.to_string()),
-        Value::Number(n) => n.as_i64().expect("cannot unwrap i64").to_string(),
-        Value::Array(values) => {
-            format!(
+struct ItemIterator<'a> {
+    content: &'a [u8],
+    working_data: &'a [u8],
+}
+
+enum Item<'a> {
+    Bytes(&'a [u8]),
+    Number(&'a [u8]),
+    List(Vec<Item<'a>>),
+    Dict(HashMap<String, Item<'a>>),
+}
+
+impl<'a> Display for Item<'a> {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Item::Bytes(bytes) => match std::str::from_utf8(bytes) {
+                Ok(value) => write!(f, "\"{}\"", value),
+                Err(_) => write!(
+                    f,
+                    "{}",
+                    bytes
+                        .iter()
+                        .map(|i| format!("{}", i))
+                        .collect::<Vec<_>>()
+                        .join("")
+                ),
+            },
+            Item::Number(number) => write!(
+                f,
+                "{}",
+                std::str::from_utf8(number).expect("can't make out number out of bytes")
+            ),
+            Item::List(items) => write!(
+                f,
                 "[{}]",
-                values
+                items
                     .iter()
-                    .map(|i| display(i))
+                    .map(|i| format!("{}", i))
                     .collect::<Vec<_>>()
                     .join(",")
-            )
-        }
-        Value::Object(values) => {
-            let mut values = values
-                .iter()
-                .map(|(k, v)| format!("{}:{}", k, display(v)))
-                .collect::<Vec<_>>();
-            values.sort_by_key(|i| i.to_owned());
-            format!("{{{}}}", values.join(","))
-        }
-        _ => todo!(),
-    }
-}
-
-#[allow(dead_code)]
-fn decode_bencoded_value(encoded_value: &str) -> Value {
-    match decode(encoded_value) {
-        Some((value, _)) => value,
-        None => panic!("can't decode value"),
-    }
-}
-
-const STRING_SEPARATOR: char = ':';
-const STRING_SEPARATOR_LEN: usize = STRING_SEPARATOR.len_utf8();
-const NUMBER_HEADER: char = 'i';
-const NUMBER_HEADER_LEN: usize = NUMBER_HEADER.len_utf8();
-const NUMBER_TRAILER: char = 'e';
-const NUMBER_TRAILER_LEN: usize = NUMBER_TRAILER.len_utf8();
-const LIST_HEADER: char = 'l';
-const LIST_HEADER_LEN: usize = LIST_HEADER.len_utf8();
-const LIST_TRAILER: char = 'e';
-const LIST_TRAILER_LEN: usize = LIST_TRAILER.len_utf8();
-const DICT_HEADER: char = 'd';
-const DICT_HEADER_LEN: usize = DICT_HEADER.len_utf8();
-const DICT_TRAILER: char = 'e';
-const DICT_TRAILER_LEN: usize = DICT_TRAILER.len_utf8();
-
-fn decode(encoded: &str) -> Option<(Value, &str)> {
-    let mut chars = encoded.chars().peekable();
-    match chars.peek() {
-        Some(c) if c.is_digit(10) => decode_string(encoded),
-        Some(&NUMBER_HEADER) => decode_int(encoded),
-        Some(&LIST_HEADER) => decode_list(encoded),
-        Some(&DICT_HEADER) => decode_dict(encoded),
-        Some(_) | None => todo!(),
-    }
-}
-
-fn decode_string(start: &str) -> Option<(Value, &str)> {
-    let header_len = start
-        .chars()
-        .take_while(|c| c.is_digit(10))
-        .map(|i| i.len_utf8())
-        .sum();
-    let len = start[0..header_len]
-        .parse::<usize>()
-        .expect("can't parse string length");
-    Some((
-        Value::String(
-            start[header_len + STRING_SEPARATOR_LEN..header_len + STRING_SEPARATOR_LEN + len]
-                .to_owned(),
-        ),
-        &start[header_len + 1 + len..],
-    ))
-}
-
-fn decode_int(start: &str) -> Option<(Value, &str)> {
-    let payload_len: usize = start
-        .chars()
-        .skip(1)
-        .take_while(|i| i != &NUMBER_TRAILER)
-        .map(|i| i.len_utf8())
-        .sum();
-    let val = start[NUMBER_HEADER_LEN..NUMBER_HEADER_LEN + payload_len]
-        .parse::<i64>()
-        .expect("can't parse int");
-    Some((
-        Value::Number(val.into()),
-        &start[NUMBER_HEADER_LEN + payload_len + NUMBER_TRAILER_LEN..],
-    ))
-}
-
-fn decode_list(start: &str) -> Option<(Value, &str)> {
-    let mut start = &start[LIST_HEADER_LEN..];
-    let mut values = Vec::new();
-    while let Some(c) = start.chars().next() {
-        if c == LIST_TRAILER {
-            break;
-        }
-        if let Some((value, rest)) = decode(start) {
-            values.push(value);
-            start = rest;
-        }
-    }
-    Some((values.into(), &start[LIST_TRAILER_LEN..]))
-}
-
-fn decode_dict(start: &str) -> Option<(Value, &str)> {
-    let mut start = &start[DICT_HEADER_LEN..];
-    let mut values = Map::new();
-    while let Some(c) = start.chars().next() {
-        if c == DICT_TRAILER {
-            break;
-        }
-        if let Some((key, rest)) = decode_string(start) {
-            if let Some((value, rest)) = decode(rest) {
-                values.insert(key.to_string(), value);
-                start = rest;
-            } else {
-                return None;
+            ),
+            Item::Dict(hashmap) => {
+                let mut hashmap = hashmap
+                    .iter()
+                    .map(|(k, v)| format!("\"{}\":{}", k, v))
+                    .collect::<Vec<_>>();
+                hashmap.sort();
+                write!(f, "{{{}}}", hashmap.join(","))
             }
-        } else {
-            return None;
+        }
+    }
+}
+
+impl<'a> ItemIterator<'a> {
+    pub fn new(content: &'a [u8]) -> Self {
+        Self {
+            content,
+            working_data: content,
         }
     }
 
-    Some((Value::Object(values), &start[DICT_TRAILER_LEN..]))
+    fn decode_bytes(&mut self) -> Item<'a> {
+        //TODO use an accumulator
+        let number_len = self
+            .working_data
+            .iter()
+            .take_while(|i| i.is_ascii_digit())
+            .count();
+        let len = std::str::from_utf8(&self.working_data[..number_len])
+            .expect("can't parse string from bytes")
+            .parse::<usize>()
+            .expect("can't parse field length");
+        let ret = Item::Bytes(&self.working_data[number_len + 1..number_len + 1 + len]);
+        self.working_data = &self.working_data[number_len + 1 + len..];
+        ret
+    }
+
+    fn decode_number(&mut self) -> Item<'a> {
+        let payload_len = self.working_data[1..]
+            .iter()
+            .take_while(|i| i != &&NUMBER_TRAILER)
+            .count();
+        let ret = Item::Number(&self.working_data[1..payload_len + 1]);
+        self.working_data = &self.working_data[1 + payload_len + 1..];
+        ret
+    }
+
+    fn decode_list(&mut self) -> Item<'a> {
+        self.working_data = &self.working_data[1..];
+        let mut items = Vec::new();
+        while self.working_data[0] != LIST_TRAILER {
+            items.push(self.decode_next());
+        }
+        self.working_data = &self.working_data[1..];
+        Item::List(items)
+    }
+
+    fn decode_dict(&mut self) -> Item<'a> {
+        self.working_data = &self.working_data[1..];
+        let mut items = HashMap::new();
+        while self.working_data[0] != DICT_TRAILER {
+            let key = match self.decode_next() {
+                Item::Bytes(bytes) => std::str::from_utf8(bytes)
+                    .expect("can't decode utf8 str from bytes")
+                    .to_owned(),
+                _ => panic!("can't decode key for dict"),
+            };
+            let value = self.decode_next();
+            items.insert(key, value);
+        }
+        Item::Dict(items)
+    }
+
+    fn decode_next(&mut self) -> Item<'a> {
+        match self.working_data[0] {
+            i if i.is_ascii_digit() => self.decode_bytes(),
+            NUMBER_HEADER => self.decode_number(),
+            LIST_HEADER => self.decode_list(),
+            DICT_HEADER => self.decode_dict(),
+            i => panic!("unknown field header '{}'", i as char),
+        }
+    }
 }
+
+impl<'a> Iterator for ItemIterator<'a> {
+    type Item = Item<'a>;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        Some(self.decode_next())
+    }
+}
+
+// struct Info {
+//     tracker: String,
+//     len: usize,
+// }
+//
+// fn info(content: &[u8]) -> Result<Info, Box<dyn Error>> {
+//     decode_dict(&content[0..124]);
+//     Ok(Info {
+//         tracker: "".to_owned(),
+//         len: 0,
+//     })
+// }
 
 fn main() {
     let args: Vec<String> = env::args().collect();
     let command = &args[1];
 
     if command == "decode" {
-        let encoded_value = &args[2];
-        let decoded_value = decode_bencoded_value(encoded_value);
-        println!("{}", display(&decoded_value));
+        let mut encoded_value = ItemIterator::new(&args[2].as_bytes());
+        println!("{}", encoded_value.next().unwrap());
     } else {
         println!("unknown command: {}", args[1])
     }
@@ -151,72 +170,98 @@ mod test {
     use super::*;
 
     #[test]
-    fn decode_string() {
-        let (val, rest) = decode("5:hello").unwrap();
-        assert_eq!("hello", val);
-        assert_eq!("", rest);
+    fn decode_simple_string() {
+        let mut iter = ItemIterator::new(b"5:hello");
+        assert_eq!(
+            Some("\"hello\"".to_owned()),
+            iter.next().map(|i| format!("{}", i))
+        );
     }
 
     #[test]
     fn decode_long_string() {
-        let (val, rest) = decode("123:Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.").unwrap();
-        assert_eq!("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.", val);
-        assert_eq!("", rest);
+        let mut iter = ItemIterator::new(b"123:Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.");
+        assert_eq!(Some("\"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.\"".to_owned()), iter.next().map(|i| format!("{}", i)));
     }
 
     #[test]
     fn decode_string_with_overflow() {
-        let (val, rest) = decode("3:foobar").unwrap();
-        assert_eq!("foo", val);
-        assert_eq!("bar", rest);
+        let mut iter = ItemIterator::new(b"3:foobar");
+        assert_eq!(
+            Some("\"foo\"".to_owned()),
+            iter.next().map(|i| format!("{}", i))
+        );
     }
 
     #[test]
     fn decode_positive_int() {
-        let (val, rest) = decode("i52e").unwrap();
-        assert_eq!("52", val.to_string());
-        assert_eq!("", rest);
+        let mut iter = ItemIterator::new(b"i52e");
+        assert_eq!(Some("52".to_owned()), iter.next().map(|i| format!("{}", i)));
     }
 
     #[test]
     fn decode_negative_int() {
-        let (val, rest) = decode("i-42e").unwrap();
-        assert_eq!("-42", val.to_string());
-        assert_eq!("", rest);
+        let mut iter = ItemIterator::new(b"i-42e");
+        assert_eq!(
+            Some("-42".to_owned()),
+            iter.next().map(|i| format!("{}", i))
+        );
     }
 
     #[test]
     fn decode_positive_int_with_overflow() {
-        let (val, rest) = decode("i52ebar").unwrap();
-        assert_eq!("52", val.to_string());
-        assert_eq!("bar", rest);
+        let mut iter = ItemIterator::new(b"i52ebar");
+        assert_eq!(Some("52".to_owned()), iter.next().map(|i| format!("{}", i)));
     }
 
     #[test]
-    fn decode_list() {
-        let (val, rest) = decode("l5:helloi52ee").unwrap();
-        assert_eq!("[\"hello\",52]", display(&val));
-        assert_eq!("", rest);
+    fn decode_simple_list() {
+        let mut iter = ItemIterator::new(b"l5:helloi52ee");
+        assert_eq!(
+            Some("[\"hello\",52]".to_owned()),
+            iter.next().map(|i| format!("{}", i))
+        );
     }
 
     #[test]
     fn decode_list_with_overflow() {
-        let (val, rest) = decode("l5:helloi52eebaz").unwrap();
-        assert_eq!("[\"hello\",52]", display(&val));
-        assert_eq!("baz", rest);
+        let mut iter = ItemIterator::new(b"l3:bazi42eebaz");
+        assert_eq!(
+            Some("[\"baz\",42]".to_owned()),
+            iter.next().map(|i| format!("{}", i))
+        );
     }
 
     #[test]
     fn decode_dict() {
-        let (val, rest) = decode("d3:foo3:bar5:helloi52ee").unwrap();
-        assert_eq!("{\"foo\":\"bar\",\"hello\":52}", display(&val));
-        assert_eq!("", rest);
+        let mut iter = ItemIterator::new(b"d3:foo3:bar5:helloi52ee");
+        assert_eq!(
+            Some("{\"foo\":\"bar\",\"hello\":52}".to_owned()),
+            iter.next().map(|i| format!("{}", i))
+        )
     }
 
     #[test]
     fn decode_dict_with_overflow() {
-        let (val, rest) = decode("d3:fooi42eebaz").unwrap();
-        assert_eq!("{\"foo\":42}", display(&val));
-        assert_eq!("baz", rest);
+        let mut iter = ItemIterator::new(b"d3:fooi42eebaz");
+        assert_eq!(
+            Some("{\"foo\":42}".to_owned()),
+            iter.next().map(|i| format!("{}", i))
+        )
     }
+
+    // #[test]
+    // fn info_file() {
+    //     let file = fs::read("tests/alice.torrent");
+    //     assert!(file.is_ok());
+    //     let info = info(&file.unwrap()).unwrap();
+    //     assert_eq!("http://disney.com/steamboat_willie", info.tracker);
+    //     assert_eq!(123, info.len);
+    // }
+    //
+    // #[test]
+    // fn info_bad_file() {
+    //     let info = info(b"foo");
+    //     assert!(info.is_err());
+    // }
 }
