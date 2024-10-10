3bddf2f22c4530b94aa0dca973b087436fe928ca
Philippe Vlérick <pvlerick@gmail.com>
2024-10-10 18:58:25 +0200

    checkpoint

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,7 @@ use std::{
 };
 
 use base64::Engine;
+use reqwest_mock::Client;
 use sha1::{Digest, Sha1};
 
 const NUMBER_HEADER: u8 = b'i';
@@ -277,6 +278,20 @@ impl Info {
     }
 }
 
+struct BtClient<T: Client> {
+    client: T,
+}
+
+impl<T: Client> BtClient<T> {
+    fn new(client: T) -> Self {
+        Self { client }
+    }
+
+    fn get_peers(&self, info: Info) -> Vec<String> {
+        self.client.get(info.tracker)
+    }
+}
+
 fn main() {
     let args: Vec<String> = env::args().collect();
     let command = &args[1];
@@ -306,6 +321,8 @@ fn main() {
 mod test {
     use super::*;
     use base64::{engine::general_purpose, Engine};
+    use reqwest::{Method, Url};
+    use reqwest_mock::{Client, StubClient, StubDefault, StubSettings, StubStrictness};
 
     fn item_from_content<'a>(content: &'a [u8]) -> Item<'a> {
         ItemIterator::new(content).next().unwrap().unwrap()
@@ -421,7 +438,7 @@ mod test {
     }
 
     #[test]
-    fn info_with_hash_and_pieces() {
+    fn info_with_hash_and_pieces_1() {
         let info = Info::parse(&general_purpose::STANDARD.decode("ZDg6YW5ub3VuY2U1NTpodHRwOi8vYml0dG9ycmVudC10ZXN0LXRyYWNrZXIuY29kZWNyYWZ0ZXJzLmlvL2Fubm91bmNlMTA6Y3JlYXRlZCBieTEzOm1rdG9ycmVudCAxLjE0OmluZm9kNjpsZW5ndGhpODIwODkyZTQ6bmFtZTE5OmNvbmdyYXR1bGF0aW9ucy5naWYxMjpwaWVjZSBsZW5ndGhpMjYyMTQ0ZTY6cGllY2VzODA6PUKiDtsc+EDNNSjTqekh22M4pGNp+IWzmIpS/7A1kZhUArbVKFlAq3aGnmycHxAflPOd4VPkaL5qY49Pve1o0C3gEaK2h/dbWDP0bM6OPpxlZQ==").unwrap()).unwrap();
         assert_eq!(
             "http://bittorrent-test-tracker.codecrafters.io/announce",
@@ -448,7 +465,54 @@ mod test {
     }
 
     #[test]
-    fn info_with_peers() {
-        assert!(false);
+    fn info_with_hash_and_pieces_2() {
+        let info = Info::parse(&general_purpose::STANDARD.decode("ZDg6YW5ub3VuY2UzMTpodHRwOi8vMTI3LjAuMC4xOjQ0MzgxL2Fubm91bmNlNDppbmZvZDY6bGVuZ3RoaTIwOTcxNTJlNDpuYW1lMTU6ZmFrZXRvcnJlbnQuaXNvMTI6cGllY2UgbGVuZ3RoaTI2MjE0NGU2OnBpZWNlczE2MDrd8zFyWZ/ahPCiCaMDT3nwuKpeInlaYYoe5SdelShDsBpWrk4UJ1Lvza4u9TLWEaRrLPe2TVeMCbOsC24Jja3AwZQ28ZJ+onuQ6xixooIKI4+lNVQZiG2exW6GzXeRND6Ted4YHK6s6xX9ETSxtLIfrQQSWyJ7Tc/6WG4g1Xmk3nYJDhK9Cj2bHFOfPq7C1+sdtTnCqdJNAj+5FreSNLdpZWU=").unwrap()).unwrap();
+        assert_eq!("http://127.0.0.1:44381/announce", info.tracker);
+        assert_eq!(2097152, info.len);
+        assert_eq!(
+            "a18a79fa44e045b1e13879166d35823e848419f8",
+            hex::encode(info.hash)
+        );
+        assert_eq!(262144, info.piece_len);
+        assert_eq!(
+            vec![
+                "ddf33172599fda84f0a209a3034f79f0b8aa5e22",
+                "795a618a1ee5275e952843b01a56ae4e142752ef",
+                "cdae2ef532d611a46b2cf7b64d578c09b3ac0b6e",
+                "098dadc0c19436f1927ea27b90eb18b1a2820a23",
+                "8fa5355419886d9ec56e86cd7791343e9379de18",
+                "1caeaceb15fd1134b1b4b21fad04125b227b4dcf",
+                "fa586e20d579a4de76090e12bd0a3d9b1c539f3e",
+                "aec2d7eb1db539c2a9d24d023fb916b79234b769"
+            ],
+            info.pieces_hashes
+                .iter()
+                .map(|i| hex::encode(i))
+                .collect::<Vec<_>>()
+        );
+    }
+
+    #[test]
+    fn info_request_peers() {
+        let info = Info::parse(&general_purpose::STANDARD.decode("ZDg6YW5ub3VuY2UzMTpodHRwOi8vMTI3LjAuMC4xOjQ0MzgxL2Fubm91bmNlNDppbmZvZDY6bGVuZ3RoaTIwOTcxNTJlNDpuYW1lMTU6ZmFrZXRvcnJlbnQuaXNvMTI6cGllY2UgbGVuZ3RoaTI2MjE0NGU2OnBpZWNlczE2MDrd8zFyWZ/ahPCiCaMDT3nwuKpeInlaYYoe5SdelShDsBpWrk4UJ1Lvza4u9TLWEaRrLPe2TVeMCbOsC24Jja3AwZQ28ZJ+onuQ6xixooIKI4+lNVQZiG2exW6GzXeRND6Ted4YHK6s6xX9ETSxtLIfrQQSWyJ7Tc/6WG4g1Xmk3nYJDhK9Cj2bHFOfPq7C1+sdtTnCqdJNAj+5FreSNLdpZWU=").unwrap()).unwrap();
+
+        let mut client = StubClient::new(StubSettings {
+            default: StubDefault::Error,
+            strictness: StubStrictness::MethodUrl,
+        });
+
+        let response = b"d8:intervali0e5:peers6:tttt00";
+        let _ = client
+            .stub(Url::parse("http://127.0.0.1:44381/announce").unwrap())
+            .method(Method::GET)
+            .response()
+            .body(response.to_vec())
+            .mock();
+
+        let res = client
+            .get("http://127.0.0.1:44381/announce")
+            .send()
+            .unwrap();
+        assert_eq!("foo", res.body_to_utf8().unwrap());
     }
 }
