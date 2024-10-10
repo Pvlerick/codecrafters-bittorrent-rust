b9cb12f761898a45080527dc1c3f7dbfce1f422e
Philippe Vlérick <pvlerick@gmail.com>
2024-10-10 09:20:31 +0200

    hashing based on the raw bytes, no reencoding

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use std::{collections::HashMap, env, error::Error, fmt::Display, fs};
 
-use base64::Engine;
+use sha1::{Digest, Sha1};
 
 const NUMBER_HEADER: u8 = b'i';
 const NUMBER_TRAILER: u8 = b'e';
@@ -15,11 +15,33 @@ struct ItemIterator<'a> {
     working_data: &'a [u8],
 }
 
+struct Field<'a, T> {
+    raw: &'a [u8],
+    payload: T,
+}
+
+impl<'a, T> Field<'a, T> {
+    fn new(raw: &'a [u8], payload: T) -> Self {
+        Self { raw, payload }
+    }
+}
+
 enum Item<'a> {
-    Bytes(&'a [u8]),
-    Number(&'a [u8]),
-    List(Vec<Item<'a>>),
-    Dict(HashMap<String, Item<'a>>),
+    Bytes(Field<'a, &'a [u8]>),
+    Number(Field<'a, &'a [u8]>),
+    List(Field<'a, Vec<Item<'a>>>),
+    Dict(Field<'a, HashMap<String, Item<'a>>>),
+}
+
+impl<'a> Item<'a> {
+    fn raw_length(&self) -> usize {
+        match self {
+            Item::Bytes(Field { raw, .. }) => raw.len(),
+            Item::Number(Field { raw, .. }) => raw.len(),
+            Item::List(Field { raw, .. }) => raw.len(),
+            Item::Dict(Field { raw, .. }) => raw.len(),
+        }
+    }
 }
 
 impl<'a> Into<String> for Item<'a> {
@@ -31,39 +53,39 @@ impl<'a> Into<String> for Item<'a> {
 impl<'a> Display for Item<'a> {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         match self {
-            Item::Bytes(bytes) => match std::str::from_utf8(bytes) {
+            Item::Bytes(Field { payload, .. }) => match std::str::from_utf8(payload) {
                 Ok(value) => write!(f, "\"{}\"", value),
                 Err(_) => write!(
                     f,
                     "{}",
-                    bytes
+                    payload
                         .iter()
                         .map(|i| format!("{}", i))
                         .collect::<Vec<_>>()
                         .join("")
                 ),
             },
-            Item::Number(number) => write!(
+            Item::Number(Field { payload, .. }) => write!(
                 f,
                 "{}",
-                std::str::from_utf8(number).expect("can't make out number out of bytes")
+                std::str::from_utf8(payload).expect("can't make out number out of bytes")
             ),
-            Item::List(items) => write!(
+            Item::List(Field { payload, .. }) => write!(
                 f,
                 "[{}]",
-                items
+                payload
                     .iter()
                     .map(|i| format!("{}", i))
                     .collect::<Vec<_>>()
                     .join(",")
             ),
-            Item::Dict(hashmap) => {
-                let mut hashmap = hashmap
+            Item::Dict(Field { payload, .. }) => {
+                let mut map = payload
                     .iter()
                     .map(|(k, v)| format!("\"{}\":{}", k, v))
                     .collect::<Vec<_>>();
-                hashmap.sort();
-                write!(f, "{{{}}}", hashmap.join(","))
+                map.sort();
+                write!(f, "{{{}}}", map.join(","))
             }
         }
     }
@@ -88,7 +110,10 @@ impl<'a> ItemIterator<'a> {
             .expect("can't parse string from bytes")
             .parse::<usize>()
             .expect("can't parse field length");
-        let ret = Item::Bytes(&self.working_data[number_len + 1..number_len + 1 + len]);
+        let ret = Item::Bytes(Field::new(
+            &self.working_data[..number_len + 1 + len],
+            &self.working_data[number_len + 1..number_len + 1 + len],
+        ));
         self.working_data = &self.working_data[number_len + 1 + len..];
         Ok(ret)
     }
@@ -98,35 +123,49 @@ impl<'a> ItemIterator<'a> {
             .iter()
             .take_while(|i| i != &&NUMBER_TRAILER)
             .count();
-        let ret = Item::Number(&self.working_data[1..payload_len + 1]);
+        let ret = Item::Number(Field::new(
+            &self.working_data[..payload_len + 2],
+            &self.working_data[1..payload_len + 1],
+        ));
         self.working_data = &self.working_data[1 + payload_len + 1..];
         Ok(ret)
     }
 
     fn decode_list(&mut self) -> Result<Item<'a>, DecodingError> {
+        let raw = self.working_data;
+        let mut end = 2;
         self.working_data = &self.working_data[1..];
         let mut items = Vec::new();
         while self.working_data[0] != LIST_TRAILER {
-            items.push(self.decode_next()?);
+            let item = self.decode_next()?;
+            end += item.raw_length();
+            items.push(item);
         }
         self.working_data = &self.working_data[1..];
-        Ok(Item::List(items))
+        Ok(Item::List(Field::new(&raw[..end], items)))
     }
 
     fn decode_dict(&mut self) -> Result<Item<'a>, DecodingError> {
+        let raw = self.working_data;
+        let mut end = 2;
         self.working_data = &self.working_data[1..];
         let mut items = HashMap::new();
         while self.working_data[0] != DICT_TRAILER {
             let key = match self.decode_next()? {
-                Item::Bytes(bytes) => std::str::from_utf8(bytes)
-                    .expect("can't decode utf8 str from bytes")
-                    .to_owned(),
+                Item::Bytes(Field { raw, payload }) => {
+                    end += raw.len();
+                    std::str::from_utf8(payload)
+                        .expect("can't decode utf8 str from bytes")
+                        .to_owned()
+                }
                 _ => return Err(DecodingError::new("can't decode key for dict")),
             };
             let value = self.decode_next()?;
+            end += value.raw_length();
             items.insert(key, value);
         }
-        Ok(Item::Dict(items))
+        self.working_data = &self.working_data[1..];
+        Ok(Item::Dict(Field::new(&raw[..end], items)))
     }
 
     fn decode_next(&mut self) -> Result<Item<'a>, DecodingError> {
@@ -173,26 +212,39 @@ impl Display for DecodingError {
 struct Info {
     tracker: String,
     len: usize,
+    hash: Vec<u8>,
 }
 
 impl Info {
     fn parse(content: &[u8]) -> Result<Info, Box<dyn Error>> {
         let mut iter = ItemIterator::new(content);
         match iter.next() {
-            Some(Ok(Item::Dict(map))) => match (map.get("announce"), map.get("info")) {
-                (Some(Item::Bytes(tracker)), Some(Item::Dict(info))) => match info.get("length") {
-                    Some(Item::Number(bytes)) => Ok(Info {
-                        tracker: std::str::from_utf8(tracker).unwrap().to_owned(),
-                        len: std::str::from_utf8(bytes)
-                            .to_owned()
-                            .unwrap()
-                            .parse()
-                            .unwrap(),
-                    }),
-                    _ => Err("bar".into()),
-                },
-                _ => Err("foo".into()),
-            },
+            Some(Ok(Item::Dict(Field { payload, .. }))) => {
+                match (payload.get("announce"), payload.get("info")) {
+                    (
+                        Some(Item::Bytes(Field {
+                            payload: tracker, ..
+                        })),
+                        Some(Item::Dict(Field { raw, payload: info })),
+                    ) => match info.get("length") {
+                        Some(Item::Number(Field { payload: bytes, .. })) => {
+                            let mut hasher = Sha1::new();
+                            hasher.update(raw);
+                            Ok(Info {
+                                tracker: std::str::from_utf8(tracker).unwrap().to_owned(),
+                                len: std::str::from_utf8(bytes)
+                                    .to_owned()
+                                    .unwrap()
+                                    .parse()
+                                    .unwrap(),
+                                hash: hasher.finalize().into_iter().collect::<Vec<_>>(),
+                            })
+                        }
+                        _ => Err("bar".into()),
+                    },
+                    _ => Err("foo".into()),
+                }
+            }
             _ => Err("bah".into()),
         }
     }
@@ -206,15 +258,10 @@ fn main() {
         let mut encoded_value = ItemIterator::new(&args[2].as_bytes());
         println!("{}", encoded_value.next().unwrap().unwrap());
     } else if command == "info" {
-        let content = fs::read(&args[2]).unwrap();
-        println!(
-            "#{}#",
-            base64::engine::general_purpose::STANDARD.encode(&content)
-        );
-
-        // let info_content = Info::parse(&fs::read(&args[2]).unwrap()).unwrap();
-        // println!("Tracker URL: {}", info_content.tracker);
-        // println!("Length: {}", info_content.len);
+        let info_content = Info::parse(&fs::read(&args[2]).unwrap()).unwrap();
+        println!("Tracker URL: {}", info_content.tracker);
+        println!("Length: {}", info_content.len);
+        println!("Info Hash: {}", hex::encode(info_content.hash));
     } else {
         println!("unknown command: {}", args[1])
     }
@@ -223,6 +270,7 @@ fn main() {
 #[cfg(test)]
 mod test {
     use super::*;
+    use base64::{engine::general_purpose, Engine};
 
     fn item_from_content<'a>(content: &'a [u8]) -> Item<'a> {
         ItemIterator::new(content).next().unwrap().unwrap()
@@ -230,65 +278,96 @@ mod test {
 
     #[test]
     fn decode_simple_string() {
-        let item = item_from_content(b"5:hello");
+        let content = b"5:hello";
+        let item = item_from_content(content);
+        assert!(matches!(item, Item::Bytes(Field {raw, ..}) if raw == content));
         assert_eq!("\"hello\"".to_owned(), format!("{}", item));
     }
 
     #[test]
     fn decode_long_string() {
-        let item = item_from_content(b"123:Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.");
+        let content = b"123:Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.";
+        let item = item_from_content(content);
+        assert!(matches!(item, Item::Bytes(Field {raw, ..}) if raw == content));
         assert_eq!("\"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.\"".to_owned(), format!("{}", item));
     }
 
     #[test]
     fn decode_string_with_overflow() {
-        let item = item_from_content(b"3:foobar");
+        let content = b"3:foobar";
+        let item = item_from_content(content);
+        assert!(matches!(item, Item::Bytes(Field {raw, ..}) if raw == &content[0..5]));
         assert_eq!("\"foo\"".to_owned(), format!("{}", item));
     }
 
     #[test]
     fn decode_positive_int() {
-        let item = item_from_content(b"i52e");
+        let content = b"i52e";
+        let item = item_from_content(content);
+        assert!(matches!(item, Item::Number(Field {raw, ..}) if raw == content));
         assert_eq!("52".to_owned(), format!("{}", item));
     }
 
     #[test]
     fn decode_negative_int() {
-        let item = item_from_content(b"i-42e");
+        let content = b"i-42e";
+        let item = item_from_content(content);
+        assert!(matches!(item, Item::Number(Field {raw, ..}) if raw == content));
         assert_eq!("-42".to_owned(), format!("{}", item));
     }
 
     #[test]
     fn decode_positive_int_with_overflow() {
-        let item = item_from_content(b"i52ebar");
+        let content = b"i52ebar";
+        let item = item_from_content(content);
+        assert!(matches!(item, Item::Number(Field {raw, ..}) if raw == &content[0..4]));
         assert_eq!("52".to_owned(), format!("{}", item));
     }
 
     #[test]
     fn decode_simple_list() {
-        let item = item_from_content(b"l5:helloi52ee");
+        let content = b"l5:helloi52ee";
+        let item = item_from_content(content);
+        assert!(matches!(item, Item::List(Field { raw, .. }) if raw == content));
         assert_eq!("[\"hello\",52]".to_owned(), format!("{}", item));
     }
 
     #[test]
     fn decode_list_with_overflow() {
-        let item = item_from_content(b"l3:bazi42eebaz");
+        let content = b"l3:bazi42eebaz";
+        let item = item_from_content(content);
+        assert!(matches!(item, Item::List(Field { raw, .. }) if raw == &content[0..11]));
         assert_eq!("[\"baz\",42]".to_owned(), format!("{}", item));
     }
 
     #[test]
     fn decode_dict() {
-        let item = item_from_content(b"d3:foo3:bar5:helloi52ee");
+        let content = b"d3:foo3:bar5:helloi52ee";
+        let item = item_from_content(content);
+        assert!(matches!(item, Item::Dict(Field { raw, .. }) if raw == content));
         assert_eq!(
             "{\"foo\":\"bar\",\"hello\":52}".to_owned(),
             format!("{}", item)
-        )
+        );
     }
 
     #[test]
     fn decode_dict_with_overflow() {
-        let item = item_from_content(b"d3:fooi42eebaz");
-        assert_eq!("{\"foo\":42}".to_owned(), format!("{}", item))
+        let content = b"d3:fooi42eebaz";
+        let item = item_from_content(content);
+        assert!(matches!(item, Item::Dict(Field { raw, .. }) if raw == &content[0..11]));
+        assert_eq!("{\"foo\":42}".to_owned(), format!("{}", item));
+    }
+
+    #[test]
+    fn decode_dict_in_dict() {
+        let content = b"d3:foo3:bar4:infod3:bari42eee";
+        let item = item_from_content(content);
+        assert!(matches!(item, Item::Dict(Field { raw, .. }) if raw == content));
+        assert_eq!(
+            "{\"foo\":\"bar\",\"info\":{\"bar\":42}}".to_owned(),
+            format!("{}", item)
+        );
     }
 
     #[test]
@@ -304,5 +383,19 @@ mod test {
     fn info_bad_file() {
         let info = Info::parse(b"foo");
         assert!(info.is_err());
+    }
+
+    #[test]
+    fn info_with_hash() {
+        let info = Info::parse(&general_purpose::STANDARD.decode("ZDg6YW5ub3VuY2U1NTpodHRwOi8vYml0dG9ycmVudC10ZXN0LXRyYWNrZXIuY29kZWNyYWZ0ZXJzLmlvL2Fubm91bmNlMTA6Y3JlYXRlZCBieTEzOm1rdG9ycmVudCAxLjE0OmluZm9kNjpsZW5ndGhpODIwODkyZTQ6bmFtZTE5OmNvbmdyYXR1bGF0aW9ucy5naWYxMjpwaWVjZSBsZW5ndGhpMjYyMTQ0ZTY6cGllY2VzODA6PUKiDtsc+EDNNSjTqekh22M4pGNp+IWzmIpS/7A1kZhUArbVKFlAq3aGnmycHxAflPOd4VPkaL5qY49Pve1o0C3gEaK2h/dbWDP0bM6OPpxlZQ==").unwrap()).unwrap();
+        assert_eq!(
+            "http://bittorrent-test-tracker.codecrafters.io/announce",
+            info.tracker
+        );
+        assert_eq!(820892, info.len);
+        assert_eq!(
+            "1cad4a486798d952614c394eb15e75bec587fd08",
+            hex::encode(info.hash)
+        );
     }
 }
